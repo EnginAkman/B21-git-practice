7edd471042ffeaed33b06aa5f84635750f216c2f
EnginAkman <usenakman@gmail.com>
2021-01-26 14:23:18 -0500

    added some more A features test

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,5 +1,6 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("awesome work for A started");
+        System.out.println("B21 learn review");
     }
 }
